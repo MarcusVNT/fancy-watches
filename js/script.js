93f820dc8e05a05571e1b88b468bd278d4901d03
MarcusVNT <marcus-vnt@live.com>
2023-08-11 23:38:28 -0300

    JS Q&A complete

--- js/script.js
+++ js/script.js
@@ -27,3 +27,23 @@ function ativarProtudo(parametros) {
 }
 
 parametros.forEach(ativarProtudo);
+
+// Q&A
+
+const perguntas = document.querySelectorAll(".perguntas button");
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  console.log(ativa);
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+function eventoPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventoPerguntas);
+console.log(perguntas);
